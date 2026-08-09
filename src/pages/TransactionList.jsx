84df4769cd64b75af8300e2214c4e704d21c115e
Hawra-14 <hawraaabdulla535@gmail.com>
2026-08-09 11:31:22 +0300

    Adds amount and classes

--- src/pages/TransactionList.jsx
+++ src/pages/TransactionList.jsx
@@ -1,13 +1,16 @@
 const TransactionList = (props) => {
-    
+
     return (
         <main>
             <h1>All Transactions</h1>
             {props.transactions.map((transaction) => (
-                <p>{transaction.name}</p>
+                <div className={transaction.isIncome ? ("income-card") : ("expense-card")}>
+                    <p>Transation name: {transaction.name}</p>
+                    <p>Transation amount: {transaction.amount}</p>
+                </div>
             ))}
         </main>
     )
 }
 
-export default TransactionList;
+export default TransactionList
