f7010fac8d53d1be309492b890e410b68b7dab48
bynbdulla <b.markhoos@gmail.com>
2026-08-11 15:35:46 +0300

    budget service

---
+++ src/services/budget.js
@@ -0,0 +1,64 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/budgets`
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const show = async (id) => {
+  const res = await fetch(`${BASE_URL}/${id}`,{
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+  })
+  const data = await res.json()
+  if (!res.ok) {
+    throw new Error(`${res.status}: ${data.message}`)
+  }
+
+  return data
+}
+
+const create = async (budgetFormData) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(budgetFormData),
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const update = async (id, updatedTransaction) => {
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedTransaction)
+  })
+  const data = await res.json()
+  if (!res.ok) {
+    throw new Error(`${res.status}: ${data.message}`)
+  }
+
+  return data
+}
+
+export {
+  index,
+  show,
+  create,
+  update,
+}
