2696d44377a1ddf724f8f76a0dd1d29d00c80d02
Hawra-14 <hawraaabdulla535@gmail.com>
2026-08-12 22:37:40 +0300

    styling

--- src/pages/TransactionList.jsx
+++ src/pages/TransactionList.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router"
+import wallet from "../assets/wallet.png"
+import expense from "../assets/expense.png"
 
 const TransactionList = (props) => {
     const [search, setSearch] = useState('')
@@ -20,28 +22,40 @@ const TransactionList = (props) => {
     return (
         <main>
             <h1>All Transactions</h1>
-            <input
-                type="text"
-                placeholder="Search transactions..."
-                value={search}
-                onChange={(event) => setSearch(event.target.value)}
-            />
-            <div>
-                <button onClick={() => setFilter('all')}>All</button>
-                <button onClick={() => setFilter('thisMonth')}>This Month</button>
-            </div>
-            <div>
-                <h3>{props.balance ? `Your Balance is ${props.balance} BD` : `Loading..`}</h3>
+            <div className="balance-filter">
+                <div class="searchbox">
+                    <input
+                        type="text"
+                        placeholder="Search transactions..."
+                        value={search}
+                        onChange={(event) => setSearch(event.target.value)}
+                    />
+                </div>
+                {/* <h3>{props.balance ? `Your Balance is ${props.balance} BD` : `Loading..`}</h3> */}
+                <div className="filters">
+                    <button className={filter === 'all' ? 'active' : 'inactive'} onClick={() => setFilter('all')}>All</button>
+                    <button className={filter === 'thisMonth' ? 'active' : 'inactive'} onClick={() => setFilter('thisMonth')}>This Month</button>
+                </div>
             </div>
             <div className="transactions">
                 {filteredTransactions.map((transaction) => (
                     transaction.userId._id === props.user._id ? (
                         <Link to={`/transactions/${transaction._id}`} key={transaction._id}>
-                            <div className={transaction.isIncome ? ("income-card") : ("expense-card")}>
-                                <p><strong>Name: {transaction.name}</strong></p>
-                                <p><strong>Amount: {transaction.amount} BD</strong></p>
-                                <p><strong>{new Date(transaction.date).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })}</strong></p>
-                            </div>
+                            {transaction.isIncome ? (
+                                <div className="income-card">
+                                    <img src={wallet} alt="income image" />
+                                    <p><strong>Name: {transaction.name}</strong></p>
+                                    <p><strong>Amount: {transaction.amount} BD</strong></p>
+                                    <p><strong>{new Date(transaction.date).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })}</strong></p>
+                                </div>
+                            ) : (
+                                <div className="expense-card">
+                                    <img src={expense} alt="expense image" />
+                                    <p><strong>Name: {transaction.name}</strong></p>
+                                    <p><strong>Amount: {transaction.amount} BD</strong></p>
+                                    <p><strong>{new Date(transaction.date).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })}</strong></p>
+                                </div>
+                            )}
                         </Link>
                     ) : (
                         false
